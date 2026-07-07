474993de059e965031b2e66c648eeca93a9915e2
Adarsh D <adarsh.d@epixelsolutions.com>
2026-07-08 00:33:19 +0530

    readme.md added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // app.js
 const express = require('express');
+const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(morgan(":method :url :status :response-time ms"));
 
 app.get('/', (req, res) => res.json({ message: 'Hello, DevOps!' }));
 app.get('/health', (req, res) => res.status(200).json({ status: 'ok' }));
